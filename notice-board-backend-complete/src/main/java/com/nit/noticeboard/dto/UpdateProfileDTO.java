0d75caaba2c305fa3625292ed0b007387ad4ad73
chandunitanp <webbanu@gmail.com>
2026-07-18 14:36:04 +0530

    Create UpdateProfileDTO.java
    added this class for edit profile

---
+++ notice-board-backend-complete/src/main/java/com/nit/noticeboard/dto/UpdateProfileDTO.java
@@ -0,0 +1,44 @@
+package com.nit.noticeboard.model;
+
+public class UpdateProfileDTO {
+
+    private String name;
+    private String gender;
+    private String phone;
+    private String personalEmail;
+
+    public UpdateProfileDTO() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPersonalEmail() {
+        return personalEmail;
+    }
+
+    public void setPersonalEmail(String personalEmail) {
+        this.personalEmail = personalEmail;
+    }
+}
